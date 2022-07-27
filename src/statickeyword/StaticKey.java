aa33922fd4b572901802931970c16638176f8f14
Romorosa <ramjay.romorosa@accenture.com>
2022-07-28 02:07:19 +0800

    static key

---
+++ src/statickeyword/StaticKey.java
@@ -0,0 +1,13 @@
+package statickeyword;
+
+public class StaticKey {
+
+	// Global variables
+	 static int a = 0; // make field variable static to have access to main method
+	
+	public static void main(String[] args) {
+		int b = 2;
+		
+		System.out.println(a);
+	}
+}
